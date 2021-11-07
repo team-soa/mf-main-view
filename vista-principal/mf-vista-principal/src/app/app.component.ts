3c21352b0b3cb1d1dfd2627a1e449c4768990c02
JUARSO <juan09fut.js@gmail.com>
2021-11-07 12:14:27 -0600

    Vista principla
    Micoservicio

--- vista-principal/mf-vista-principal/src/app/app.component.ts
+++ vista-principal/mf-vista-principal/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mf-vista-principal',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'mf-vista-principal';
